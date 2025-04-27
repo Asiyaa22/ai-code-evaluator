5c8f06eaec259e4ebca0419eae7e00145c1b805b
Asiya Farhath <asiyacode@gmail.com>
2025-04-28 03:48:27 +0530

    updated

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 import env from "dotenv";
-import { loadRubric } from "./utils.js"
-import { extractZip } from "./utils.js"
-import { evaluateAllSubmissions } from "./evaluate.js"
+import { clearFolder } from "./clearFolder.js";
+import { loadRubric } from "./utils.js";
+import { extractZip } from "./utils.js";
+import { evaluateAllSubmissions } from "./evaluate.js";
 
 env.config();
 
 const run = async () => {
-    console.log("Starting ai code evaluator🚀");
+  console.log("Starting ai code evaluator🚀");
 
-    //load rubric from YAML
-    const rubric = loadRubric("./rubric.yaml");
-    console.log("Loaded Rubric:", rubric);
+  //load rubric from YAML
+  const rubric = loadRubric("./rubric.yaml");
+  console.log("Loaded Rubric:", rubric);
 
-    //extract zip file
-    await extractZip("./student-submissions.zip", "./submissions");
-    console.log("Zip files extracted");
+  // Before starting evaluation clearing if folders exist
+  clearFolder("./submissions");
+  clearFolder("./outputs");
 
-    //evaluate submissions
-    await evaluateAllSubmissions("./submissions", rubric);
-    console.log("Evaluations are also done")
+  //extract zip file
+  await extractZip("./student-submissions.zip", "./submissions");
+  console.log("Zip files extracted");
 
+  //evaluate submissions
+  await evaluateAllSubmissions("./submissions", rubric);
+  console.log("Evaluations are also done");
 
-    console.log("✅ Done! Results saved in /outputs folder.");
-}
+  console.log("✅ Done! Results saved in /outputs folder.");
+};
 
 run();
