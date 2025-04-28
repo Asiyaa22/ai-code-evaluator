89602f823cebb2264945be85202e80f2b1c9be97
Asiya Farhath <asiyacode@gmail.com>
2025-04-28 12:24:28 +0530

    fixed cors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
-import multer from "multer";
 import cors from "cors"; 
+import multer from "multer";
 import fs from "fs";
 import path from "path";
 import { clearFolder } from "./clearFolder.js";
